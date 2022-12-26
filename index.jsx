abd31d5a9bd11667439b7f297bfb1755dbf4a7a5
Antoine Labard <antoine@labard.fr>
2022-12-26 14:14:12 +0100

    feat: 🏗️ Define the general project architecture

---
+++ index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+ 
+ReactDOM.render(
+    <React.StrictMode>
+        <Router>
+            <Route path="/">
+                <Home />
+            </Route>
+            <Route path="/404">
+                <NotFound />
+            </Route>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/housing">
+                <Housing />
+            </Route>
+        </Router>
+    </React.StrictMode>
+)
