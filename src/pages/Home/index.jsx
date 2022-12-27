49fb66f843dc7a53a7ae16288bb1f27fc561ce24
Antoine Labard <antoine@labard.fr>
2022-12-27 09:30:29 +0100

    feat: 🚧 implement a prototype of the home page
    logements.js are all the mock data that has been convert in js format to import them easily while testing the components.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,26 @@
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import homeBackground from "../../assets/home_background.jpg"
+
+import housings from '../../logements.js'
+
 function Home() {
-    return (
-        <div>Home</div>
-    )
+  
+  console.log("housings : " + housings)
+  return (
+    <div>
+      <Header />
+      <img src={homeBackground} alt="home background cover" />
+      <section>
+        <ul>
+          {housings.map((housing) => (
+            <li>{JSON.stringify(housing)}</li>
+          ))}
+        </ul>
+      </section>
+      <Footer />
+    </div>
+  )
 }
 
 export default Home
