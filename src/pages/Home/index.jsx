9650f96643868629cabe50705795d5872785ffe8
Antoine Labard <antoine@labard.fr>
2022-12-29 16:09:13 +0100

    feat: ✨ implement the Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,23 +1,30 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import homeBackground from "../../assets/home_background.jpg"
+import HomeCard from "../../components/HomeCard"
+import "./index.scss"
 
-import housings from '../../logements.js'
+import homes from "../../logements.js"
 
 function Home() {
-  
-  console.log("housings : " + housings)
   return (
     <div>
       <Header />
-      <img src={homeBackground} alt="home background cover" />
-      <section>
-        <ul>
-          {housings.map((housing) => (
-            <li>{JSON.stringify(housing)}</li>
-          ))}
-        </ul>
-      </section>
+      <main>
+        <div className="cover">
+          <h1>Chez vous, partout et ailleurs</h1>
+          <img src={homeBackground} alt="home background cover" />
+        </div>
+        <section className="homeCardsList">
+          <ul>
+            {homes.map((home) => (
+              <li>
+                <HomeCard home={home} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </main>
       <Footer />
     </div>
   )
