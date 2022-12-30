c349e5f06ab769cdca00ec72d0da40178e43859f
Antoine Labard <antoine@labard.fr>
2022-12-30 15:20:37 +0100

    feat: ✨ implement About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,27 @@
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+import aboutCover from "../../assets/about-cover.jpg"
+import CollapseCard from "../../components/CollapseCard"
+import "./index.scss"
+
 function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <div>
+      <Header />
+      <main>
+        <div className="cover">
+          <img src={aboutCover} alt="about page cover" />
+        </div>
+        <section>
+          <CollapseCard title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
+          <CollapseCard title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
+          <CollapseCard title="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
+          <CollapseCard title="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+        </section>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default About
