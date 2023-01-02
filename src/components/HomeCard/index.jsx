b75ecd507604f7ee0e885a6749325e2a57ace424
Antoine Labard <antoine@labard.fr>
2023-01-02 09:54:52 +0100

    feat: ✨ make the home cards clickable to redirect to their corresponding housing's page

--- src/components/HomeCard/index.jsx
+++ src/components/HomeCard/index.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 
 function HomeCard({home}) {
     return (
-        <div className="homeCard">
+        <a href={`housing/${home.id}`} className="homeCard">
             <img src={home.cover} alt={home.cover} />
             <p>{home.title}</p>
-        </div>
+        </a>
     )
 }
 
