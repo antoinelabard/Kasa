623d30bc01f1834ca3a892649f190d4544a5d7ce
Antoine Labard <antoine@labard.fr>
2023-01-02 17:01:09 +0100

    feat: ✨ add Rating component

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,8 +1,19 @@
-import { Link } from 'react-router-dom'
- 
-function Rating() {
-    return (
-    )
+import "./index.scss"
+import starActive from "../../assets/star-active.svg"
+import starInactive from "../../assets/star-inactive.svg"
+
+function Rating({ rating }) {
+  const baseRating = [1, 2, 3, 4, 5]
+  return (
+    <div className="rating">
+      {baseRating.map((r) => (
+        <img
+          src={r <= rating ? starActive : starInactive}
+          alt="rating star"
+        />
+      ))}
+    </div>
+  )
 }
 
 export default Rating
