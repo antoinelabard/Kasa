41afaac44250526b514349180007363648d78a05
Antoine Labard <antoine@labard.fr>
2023-01-02 18:17:57 +0100

    feat: ✨ implement Housing page

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -1,13 +1,52 @@
 import { useParams } from "react-router-dom"
 import Carousel from "../../components/Carousel"
-import homes from '../../logements.js'
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import Repository from "../../data/Repository"
+import NotFound from "../NotFound"
+import Chip from "../../components/Chip"
+import Hero from "../../components/Hero"
+import Rating from "../../components/Rating"
+import CollapseCard from "../../components/CollapseCard"
+import "./index.scss"
 
 function Housing() {
-    const {id} = useParams()
-    console.log(id)
-    return (
-        <div>Housing</div>
-    )
+  const { id } = useParams()
+  const housing = new Repository().getHousingById(id)
+  if (!housing) {
+    return <NotFound />
+  }
+  return (
+    <>
+      <Header />
+      <main>
+        <Carousel housing={housing} />
+        <section className="housing-info">
+          <div className="housing-info__general">
+            <h1>{housing.title}</h1>
+            <h2>{housing.location}</h2>
+            <div>
+              {housing.tags.map((tag) => (
+                <Chip text={tag} />
+              ))}
+            </div>
+          </div>
+          <div className="housing-info__host">
+            <Hero host={housing.host} />
+            <Rating rating={housing.rating} />
+          </div>
+        </section>
+        <section className="housing-description">
+          <CollapseCard title="Description" description={housing.description} />
+          <CollapseCard
+            title="Équipements"
+            description={housing.equipments.join("\n")}
+          />
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default Housing
