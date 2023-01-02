5050389bb8d8f0127dc4658fe8759e04655367ce
Antoine Labard <antoine@labard.fr>
2023-01-02 09:10:59 +0100

    feat: ✨ implement NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home/index.jsx"
 import About from "./pages/About/index"
 import Housing from "./pages/Housing/index"
+import NotFound from "./pages/NotFound/index.jsx"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/housing/:id" element={<Housing />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </Router>
   )
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,19 @@
+import Footer from "../../components/Footer"
+import Header from "../../components/Header"
+import "./index.scss"
+
 function NotFound() {
-    return (
-        <div></div>
-    )
+  return (
+    <div>
+      <Header />
+      <main className="notFound">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <a href="/">Retourner à la page d'accueil</a>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default NotFound
