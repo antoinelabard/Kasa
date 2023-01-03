9e45f868234d9b6d1f32584cd908b70aac13543c
Antoine Labard <antoine@labard.fr>
2023-01-03 17:14:01 +0100

    fix: 🐛 bad use of a styled component

--- src/components/CollapseCard/index.jsx
+++ src/components/CollapseCard/index.jsx
@@ -1,29 +1,23 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
 import collapseArrow from "../../assets/collapse-arrow.svg"
-import styled from 'styled-components'
+import styled from "styled-components"
 import "./index.scss"
+
+const CollapseArrowStyle = styled.img`
+  ${(props) => props.isCollapsed && `transform: rotate(180deg)`});
+`
 
 function CollapseCard({ title, description }) {
   const [isCollapsed, setCollapse] = useState(true)
-
-  const CollapseArrowStyle = styled.img`
-  transform: rotate(${
-    isCollapsed ? "0" : "180deg"
-  });
-    `
 
   return (
     <div className="collapseContainer">
       <button onClick={() => setCollapse(!isCollapsed)}>
         <h2>{title}</h2>
-        <CollapseArrowStyle src={collapseArrow} alt="Collapse arrow" />
+        <CollapseArrowStyle isCollapsed='true' src={collapseArrow} alt="Collapse arrow" />
       </button>
-      {!isCollapsed && (
-        <section>
-          {description}
-        </section>
-      )}
+      {!isCollapsed && <section>{description}</section>}
     </div>
   )
 }
