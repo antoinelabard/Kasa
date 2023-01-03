6d26129ab5caf67ad55fe4450a1d460082440119
Antoine Labard <antoine@labard.fr>
2023-01-03 12:18:11 +0100

    feat: 💄 improve UI according to the mockup for About page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -12,11 +12,33 @@ function About() {
         <div className="about-cover">
           <img src={aboutCover} alt="about page cover" />
         </div>
-        <section>
-          <CollapseCard title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." />
-          <CollapseCard title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
-          <CollapseCard title="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." />
-          <CollapseCard title="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
+        <section className="collapse-cards">
+          <ul>
+            <li>
+              <CollapseCard
+                title="Fiabilité"
+                description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+              />
+            </li>
+            <li>
+              <CollapseCard
+                title="Respect"
+                description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+              />
+            </li>
+            <li>
+              <CollapseCard
+                title="Service"
+                description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+              />
+            </li>
+            <li>
+              <CollapseCard
+                title="Sécurité"
+                description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+              />
+            </li>
+          </ul>
         </section>
       </main>
       <Footer />
